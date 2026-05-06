f3146f5a06f3e7494546c3377a1e315795df843b
graysonbyrd <byrdgb1@gmail.com>
2026-05-06 14:26:02 +0000

    Fixing formatting issues

--- lua/formatting.lua
+++ lua/formatting.lua
@@ -1,32 +1,33 @@
--- Install plugin
 vim.pack.add({
   { src = "https://github.com/stevearc/conform.nvim" },
 })
 
--- Setup
 local conform = require("conform")
 
 conform.setup({
+  formatters = {
+    black = {
+      args = { "--target-version", "py310", "--quiet", "-" },
+    },
+  },
   formatters_by_ft = {
     html = { "prettier" },
     json = { "prettier" },
     yaml = { "prettier" },
     lua = { "stylua" },
     python = { "isort", "black" },
-    -- python = { "isort" },
   },
   format_on_save = {
-    lsp_fallback = true,
+    lsp_format = "fallback",
     async = false,
-    timeout_ms = 1000,
+    timeout_ms = 5000,
   },
 })
 
--- Keymap
 vim.keymap.set({ "n", "v" }, "<leader>mp", function()
   conform.format({
-    lsp_fallback = true,
+    lsp_format = "fallback",
     async = false,
-    timeout_ms = 1000,
+    timeout_ms = 5000,
   })
 end, { desc = "Format file or range (in visual mode)" })
